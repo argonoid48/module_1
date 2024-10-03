cec1d28622b623e56d2a8d1fa48ab9b0302fc3bd
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-10-03 22:24:42 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,8 @@
+# Практическая работа по теме "Переменные"
+# Андрей Галкин
+
+volume_homework = 12
+volume_time = 1.5
+mame_lesson ='Python'
+one_lesson_time = volume_time/volume_homework
+print('Курс: '+mame_lesson,', всего задач:',volume_homework,', затрачено часов:',volume_time,', среднее время выполнения', one_lesson_time, 'часа.')
