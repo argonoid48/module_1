26d06b23c4b8c4f68d8913005930034d466530b7
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-10-03 20:07:12 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+# Практическая работа по уроку "Динамическая типизация"
+# Андрей Галкин
+
+name ='Андрей'
+print('Name: ' + name)
+age = 59
+print( 'Age:', age)
+age = (age +5)
+print('New Age:', age)
+is_student = True
+print('Is Stedent:', is_student)
+
